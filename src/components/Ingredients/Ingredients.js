e55ae5a5a22657ea3d3608d238068142ac07acc0
User <user@Users-MacBook-Pro.local>
2019-07-22 15:39:17 -0500

    finished redux1

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import store, { ADD_INGREDIENT } from "./../../store";
 import { Link } from "react-router-dom";
 
 class Ingredients extends Component {
   constructor(props) {
     super(props);
+    const reduxState = store.getState();
     this.state = {
-      ingredients: [],
+      ingredients: reduxState.ingredients,
       input: ""
     };
   }
@@ -14,12 +16,27 @@ class Ingredients extends Component {
       input: val
     });
   }
+
+  componentDidMount() {
+    store.subscribe(() => {
+      const reduxState = store.getState();
+      this.setState({
+        ingredients: reduxState.ingredients
+      });
+    });
+  }
+
   addIngredient() {
     // Send data to Redux state
+    store.dispatch({
+      type: ADD_INGREDIENT,
+      payload: this.state.input
+    });
     this.setState({
       input: ""
     });
   }
+
   render() {
     const ingredients = this.state.ingredients.map((ingredient, i) => {
       return <li key={i}>{ingredient}</li>;
@@ -28,17 +45,14 @@ class Ingredients extends Component {
       <div className="List forms">
         <h2>Ingredients:</h2>
         <div className="form_items_container">
-          <ul className='list'>{ingredients}</ul>
+          <ul className="list">{ingredients}</ul>
         </div>
         <div className="add_container">
           <input
             value={this.state.input}
             onChange={e => this.handleChange(e.target.value)}
           />
-          <button
-            className="add_button"
-            onClick={() => this.addIngredient()}
-          >
+          <button className="add_button" onClick={() => this.addIngredient()}>
             Add Ingredient
           </button>
         </div>
--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import store, { ADD_INSTRUCTION, ADD_RECIPE } from "./../../store";
 
 class Instructions extends Component {
   constructor(props) {
     super(props);
+    const reduxState = store.getState();
     this.state = {
-      instructions: [],
+      instructions: reduxState.instructions,
       input: ""
     };
+  }
+
+  componentDidMount() {
+    store.subscribe(() => {
+      const reduxState = store.getState();
+      this.setState({
+        instructions: reduxState.instructions
+      });
+    });
   }
   handleChange(val) {
     this.setState({
@@ -16,12 +27,19 @@ class Instructions extends Component {
   }
   addInstruction() {
     // Send data to Redux state
+    store.dispatch({
+      type: ADD_INSTRUCTION,
+      payload: this.state.input
+    });
     this.setState({
       input: ""
     });
   }
   create() {
     // Create new recipe in Redux state
+    store.dispatch({
+      type: ADD_RECIPE
+    });
   }
   render() {
     const instructions = this.state.instructions.map((instruction, i) => {
@@ -31,7 +49,7 @@ class Instructions extends Component {
       <div className="List forms">
         <h2>Instructions:</h2>
         <div className="form_items_container">
-          <ol className='list'>{instructions}</ol>
+          <ol className="list">{instructions}</ol>
         </div>
         <div className="add_container">
           <input
@@ -43,10 +61,12 @@ class Instructions extends Component {
           </button>
         </div>
         <Link to="/add/ingredients">
-          <button className='left_button'>Previous</button>
+          <button className="left_button">Previous</button>
         </Link>
         <Link to="/">
-          <button className='right_button' onClick={() => this.create()}>Create</button>
+          <button className="right_button" onClick={() => this.create()}>
+            Create
+          </button>
         </Link>
       </div>
     );
